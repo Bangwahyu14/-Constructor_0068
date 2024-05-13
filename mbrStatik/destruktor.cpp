b5b2e0a8cd771ae21f3b6a6c896e5fc5b5f9c6dc
Bangwahyu14 <whagung553@gmail.com>
2024-05-13 17:19:08 +0700

    membuat void angka

--- mbrStatik/destruktor.cpp
+++ mbrStatik/destruktor.cpp
@@ -24,3 +24,9 @@ angka::~angka() {//DEstructor
 	delete[]arr;
 	cout << "Alamat Array Sudah Dilepaskan" << endl;
 }
+
+void angka::cetakData() {
+	for (int i = 1; i <= panjang; i++) {
+		cout << i << " = " << arr[i] << endl;
+	}
+}
