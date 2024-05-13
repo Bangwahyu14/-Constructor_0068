19ce02fafac2e3f3facf6486e16b58cb7a69b724
Bangwahyu14 <whagung553@gmail.com>
2024-05-13 15:47:24 +0700

    memindahkan di luar kelas

--- mbrStatik/constructor.cpp
+++ mbrStatik/constructor.cpp
@@ -16,36 +16,38 @@ public:
 	{
 		nim = iNim;
 	};
-	Mahasiswa(string);
+	Mahasiswa(string iNama);
 	Mahasiswa(int iNim, string iNama);
 	void cetak() {
 		cout << "NIM  =" << nim << endl;
 		cout << "Nama =" << nama << endl;
 		cout << endl;
 	}
-	Mahasiswa::Mahasiswa(string iNama) {
-		nim = 0;
-		nama = iNama;
-	}
 
-	Mahasiswa::Mahasiswa(int iNim, string iNama)
-	{
-		nim = iNim;
-		nama = iNama;
-	};
-	int main()
-	{
-		Mahasiswa mhs1;
-		Mahasiswa mhs2(123);
-		Mahasiswa mhs3("wahyu agung");
-		Mahasiswa mhs4(15,"wahyu");
-
-		mhs1.cetak();
-
-		mhs2.cetak();
-		mhs3.cetak();
-		mhs4.cetak();
-		return 0;
-	}
 
 };
+
+Mahasiswa::Mahasiswa(string iNama) {
+	nim = 0;
+	nama = iNama;
+}
+
+Mahasiswa::Mahasiswa(int iNim, string iNama)
+{
+	nim = iNim;
+	nama = iNama;
+};
+int main()
+{
+	Mahasiswa mhs1;
+	Mahasiswa mhs2(123);
+	Mahasiswa mhs3("wahyu agung");
+	Mahasiswa mhs4(15, "wahyu");
+
+	mhs1.cetak();
+
+	mhs2.cetak();
+	mhs3.cetak();
+	mhs4.cetak();
+	return 0;
+}
